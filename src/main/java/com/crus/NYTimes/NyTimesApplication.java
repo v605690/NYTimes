f03521173ae6fbe7e302130036424f2fac05e674
v605690 <v605690@gmail.com>
2025-08-01 10:47:44 -0500

    add new Java project NYTimes

---
+++ src/main/java/com/crus/NYTimes/NyTimesApplication.java
@@ -0,0 +1,13 @@
+package com.crus.NYTimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyTimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyTimesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crus/NYTimes/NyTimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crus.NYTimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyTimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
